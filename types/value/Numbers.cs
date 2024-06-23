5bd92a5ca1e44bda7d30c9bcb7dc8c7e83d65061
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2024-06-23 08:21:42 -0300

    add numeric types declaration

---
+++ types/value/Numbers.cs
@@ -0,0 +1,10 @@
+// Integer literals
+uint uintNumber = 123U;      // Unsigned int
+long longNumber = 123L;      // Long
+ulong ulongNumber = 123UL;   // Unsigned long
+
+// Floating-point literals
+float floatNumber = 123.45F; // Float
+double doubleNumber = 123.45; // Double (default for floating-point literals)
+double doubleNumberD = 123.45D; // Double (explicitly specified)
+decimal decimalNumber = 123.45M; // Decimal
