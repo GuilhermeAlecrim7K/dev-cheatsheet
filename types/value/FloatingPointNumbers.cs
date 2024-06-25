94c6f04cd33ef2a7cfda07606bdfc738003fe386
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2024-06-24 21:59:12 -0300

    add more information to FloatingPointNumbers.cs

--- types/value/FloatingPointNumbers.cs
+++ types/value/FloatingPointNumbers.cs
@@ -1,5 +1,6 @@
 /*
     float
+        - Stored in binary format
         - Ranges from 1.5 x 10⁻⁴⁵ to 3.4 x 10³⁸
         - 7 digit precision
 */
@@ -7,13 +8,15 @@ float myFloatLiteral = 0.25F;
 
 /*
     double
+        - Stored in binary format
         - Ranges from 5.0 x 10⁻³²⁴ to 1.7 x 10³⁰⁸
         - 15~16 digit precision
 */
-double myDoubleLiteral = 0.625;
+double myDoubleLiteral = 0.625; // or 0.625D
 
 /*
     decimal
+        - Stored in decimal format. Large memory footprint with more precision.
         - Ranges from -7.9 x 10⁻²⁸ to 7.9 x 10²⁸
         - 28 digit precision
 */
@@ -38,3 +41,8 @@ Console.WriteLine($"{myValue:N2}"); // 12,345.68
 
 //P => percentage
 Console.WriteLine($"{myValue:P2}"); // 1,234,567.89 %
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
