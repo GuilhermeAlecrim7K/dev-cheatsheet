a79e9e7ce3310861407beb46715adcd3090851b6
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2024-08-17 20:05:21 -0300

    add Exceptions.cs

---
+++ csharp/utils/Exceptions.cs
@@ -0,0 +1,15 @@
+/*
+Reference materials
+
+    You can find additional information about using exceptions here: https://learn.microsoft.com/dotnet/csharp/fundamentals/exceptions/using-exceptions.
+
+    You can find additional information about creating and throwing here: https://learn.microsoft.com/dotnet/csharp/fundamentals/exceptions/creating-and-throwing-exceptions.
+
+    You can find additional information about creating user defined exceptions here: https://learn.microsoft.com/dotnet/standard/exceptions/how-to-create-user-defined-exceptions.
+
+    You can find additional information about localized exception messages here: https://learn.microsoft.com/dotnet/standard/exceptions/how-to-create-localized-exception-messages.
+
+    You can find additional information about explicitly throwing exceptions here: https://learn.microsoft.com/dotnet/standard/exceptions/how-to-explicitly-throw-exceptions.
+
+    You can find additional information about using specific exceptions here: https://learn.microsoft.com/dotnet/standard/exceptions/how-to-use-specific-exceptions-in-a-catch-block.
+*/
